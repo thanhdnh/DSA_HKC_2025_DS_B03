e9430d93aa1210cd693cdbf38578fa0a2f19b757
thanhdnh <myhoangthanh@yahoo.com>
2025-10-24 09:33:43 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ public class MyStack{
         newnode.next = top;
         top = newnode;
     }
-    public object Pop(){
+    public object Pop()
+    {
         if (IsEmpty())
             return null;
         else
@@ -22,6 +23,58 @@ public class MyStack{
             top = top.next;
             return result.data;
         }
+    }
+    public object Peek()
+    {
+        if (IsEmpty())
+            return null;
+        else
+            return top.data;
+    }
+    public bool IsExist(object data)
+    {
+        if (this.IsEmpty())
+            return false;
+        MyStack tempStack = new MyStack();
+        bool found = false;
+        while (this.IsEmpty() == false)
+        {
+            object temp = this.Pop();
+            tempStack.Push(temp);
+            if (temp.Equals(data))
+            {
+                found = true; break;
+            }
+        }
+        while (tempStack.IsEmpty() == false)
+            this.Push(tempStack.Pop());
+        return found;
+    }
+    public void Swap(object a, object b)
+    {
+        bool foundA = this.IsExist(a);
+        bool foundB = this.IsExist(b);
+        if(foundA == false || foundB == false)
+            return;
+        MyStack tempStack = new MyStack();
+        //while (this.IsEmpty() == false)
+        object t;
+        do
+        {
+            t = this.Pop();
+            tempStack.Push(t);
+        } while (t.Equals(b)==false);
+
+        while (tempStack.IsEmpty() == false){
+            object temp = tempStack.Pop();
+            if (temp.Equals(a))
+                this.Push(b);
+            else if (temp.Equals(b))
+                this.Push(a);
+            else
+                this.Push(temp);
+        }
+        
     }
 }
 internal class Program
